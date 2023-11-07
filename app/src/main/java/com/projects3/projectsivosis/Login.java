c12cf0bacd385e4985c0911ed3a5f6ae744f563d
YuzuKanaeru <alfandyputra05@gmail.com>
2023-11-07 22:28:17 +0700

    fourth commit

--- app/src/main/java/com/projects3/projectsivosis/Login.java
+++ app/src/main/java/com/projects3/projectsivosis/Login.java
@@ -1,44 +1,64 @@
 package com.projects3.projectsivosis;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 public class Login extends AppCompatActivity {
 
-    private Button btnLogin,btnScan;
+
+    private Button btnLogin, btnScan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        btnLogin = findViewById(R.id.btnlogin);
-        btnScan = findViewById(R.id.btnScan);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent login = new Intent(Login.this,Petunjuk.class);
-                startActivity(login);
-            }
-        });
+        btnScan = findViewById(R.id.btnScan);
 
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Scanner();
+                IntentIntegrator integrator = new IntentIntegrator(Login.this);
+                integrator.initiateScan();
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            // Hasil pemindaian QR code
+            String qrCodeData = result.getContents();
+            if (qrCodeData.equals("12345")) {
+                Intent intent = new Intent(Login.this,Petunjuk.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Kode tidak valid", Toast.LENGTH_SHORT).show();
+                // Proses data QR code sesuai kebutuhan aplikasi Anda
+            }
+            } else{
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+        }
+
+
 
     private void Scanner() {
         ScanOptions options = new ScanOptions();
--- app/src/main/java/com/projects3/projectsivosis/StartScan.java
+++ app/src/main/java/com/projects3/projectsivosis/StartScan.java
@@ -1,7 +1,5 @@
 package com.projects3.projectsivosis;
 
-import android.app.Activity;
-
 import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class StartScan extends CaptureActivity {
