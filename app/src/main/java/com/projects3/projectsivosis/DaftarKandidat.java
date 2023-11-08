586c3ba295d1623e02e4728f6f3bed0d213b9a8a
YuzuKanaeru <alfandyputra05@gmail.com>
2023-11-08 23:07:10 +0700

    fifth commit

--- app/src/main/java/com/projects3/projectsivosis/DaftarKandidat.java
+++ app/src/main/java/com/projects3/projectsivosis/DaftarKandidat.java
@@ -1,30 +1,25 @@
 package com.projects3.projectsivosis;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DaftarKandidat extends AppCompatActivity {
 
-    private Button btnKndt1;
+    ImageView detailKandidat;
+    TextView daftarKandidat;
+    Button pilih;
 
 @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.daftarkandidat);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.daftarkandidat);
 
-        btnKndt1 = findViewById(R.id.btnkndt1);
-
-        btnKndt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent kekndt1 = new Intent(DaftarKandidat.this,DetailKandidat.class);
-                startActivity(kekndt1);
-                }
-
-        });
-    }
+}
 }
